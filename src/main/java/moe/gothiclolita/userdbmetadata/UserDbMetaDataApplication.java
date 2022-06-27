ddef3b5d922c378590fd9644083d7eff23fa0fec
luoshieryi <fhaoze@gmail.com>
2022-06-27 20:45:08 +0800

    init projects

---
+++ src/main/java/moe/gothiclolita/userdbmetadata/UserDbMetaDataApplication.java
@@ -0,0 +1,13 @@
+package moe.gothiclolita.userdbmetadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDbMetaDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserDbMetaDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/moe/gothiclolita/userdbmetadata/UserDbMetaDataApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.gothiclolita.userdbmetadata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDbMetaDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
